bf9b61194068a25c7be568e206b77778cd62154a
Иванова Дая Аскеровна <iv622s08@lwp11.csc.local>
2017-02-17 16:48:32 +0700

    Реализация функции проверки входных данных на корректность

--- deposit.c
+++ deposit.c
@@ -3,10 +3,16 @@ void input(int *p, int *s){
 	printf("Input period and sum deposit: ");
 	scanf("%d%d", &(*p), &(*s));
 	}
-
+int check(int p, int s){
+	if ((p > 365 || p < 0) || (s < 10000)){
+		printf("Error");
+		return -1;
+	}
+	return 0;
+}
 int main(){
 	int p = 0, s = 0;
 	input(&p, &s);
-	printf("%d %d", p, s);
+	check(p, s);
 	return 0;
 }
