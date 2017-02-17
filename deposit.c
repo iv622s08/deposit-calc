36cfff58f52df4abca068ab20442bf6d40ca7915
Иванова Дая Аскеровна <iv622s08@lwp11.csc.local>
2017-02-17 16:59:33 +0700

    CodeStyle

--- deposit.c
+++ deposit.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-void input(int *p, int *s){
+void input(int *p, int *s)
+{
 	printf("Input period and sum deposit: ");
 	scanf("%d%d", &(*p), &(*s));
-	}
-int check(int p, int s){
+}
+int check(int p, int s)
+{
 	if ((p > 365 || p < 0) || (s < 10000)){
 		printf("Error");
 		return -1;
 	}
 	return 0;
 }
-int main(){
+int main()
+{
 	int p = 0, s = 0;
 	input(&p, &s);
 	check(p, s);
